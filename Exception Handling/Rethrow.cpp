ac77479c927d00ed654c2ec42426d357028b7559
dhirajsahh <dhirajmahato2019@gmail.com>
2023-07-30 13:09:59 +0545

    Rethrow added

---
+++ Exception Handling/Rethrow.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int x,a,b;
+   cout<<"Enter the value of a and b"<<endl;
+   cin>>a>>b;
+   x=a-b;
+   try{
+   	if(x!=0){
+   	cout<<"Dividing number by their differece "<<(a+b)/x<<endl;
+	   }
+	   else {
+	   		throw x;
+	   	
+	   }
+   }
+   catch(int n){
+   	cout<<"The number cannnot be used as divider "<<n<<endl;
+   	throw;
+   }
+   return 0;
+}
